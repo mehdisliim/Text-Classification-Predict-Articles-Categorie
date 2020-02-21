11b79bc58f04cdd85f506b0dce8ea69434ba58aa
Mehdi Slim <59410152+mehdisliim@users.noreply.github.com>
2020-02-21 22:22:17 +0100

    ''

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+watch this video to see the model on action : 
+https://youtu.be/WUeLptHeQQY
+
+
+Note:
+-if you want to retrain the model or make predictions make sure that the location
+of the files (Data, Model and Transformers) are correct.
---
+++ test_on_new_article/predict.py
@@ -0,0 +1,26 @@
+from joblib import load
+from keras.models import load_model
+import pandas as pd, numpy as np, os
+from keras.preprocessing.text import text_to_word_sequence
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+stopwords = stopwords.words('english')
+os.chdir('/Users/mehdi/Downloads/TextClassification/test_on_new_article/model_and_transformers/')
+ohot_enc, vectorizer, model = load('OneHotEncoder.SKLEARN'), load('CountVectorizer.SKLEARN'), load_model('model.KERAS')
+os.chdir('/Users/mehdi/Downloads/TextClassification/test_on_new_article/')
+def predict(article_name='insert_the_article_text_here.txt'):
+    try:
+        sent = open(article_name).read().replace('\n', ' ')
+    except:
+        sent = ' '.join([df.columns[0]]+ df[df.columns[0]].tolist())
+    sent = text_to_word_sequence(sent,  filters='!"#%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n0123456789')
+    sent = [word for word in sent if word not in stopwords]
+    sent = [WordNetLemmatizer().lemmatize(word) for word in sent]
+    sent = [PorterStemmer().stem(word) for word in sent]
+    sent = vectorizer.transform(sent).toarray()
+    vec = sent.sum(axis=0)
+    vec = list(vec) + [0 for i in range(59)]
+    vec = np.array(vec).reshape(1, 10, 1900)
+    y_pred = model.predict(np.c_[vec])
+    category = ohot_enc.inverse_transform(np.array(y_pred))
+    print("The Article's Category is : "+str(category[0][0].capitalize())) 
